0bc71c6d8834cb7b70e5c8861ee567a19cea6482
Zeljko S <zsvedic@gmail.com>
2019-12-26 14:54:31 +0100

    Fixed PDF size, gutter and line spacing

--- md2all.bat
+++ md2all.bat
@@ -1,3 +1,3 @@
 pandoc -s EBH.md -o out-EBH.html --toc -c html-epub-styling.css --lua-filter=pagebreak.lua 
 pandoc -s EBH.md -o out-EBH.epub --toc -c html-epub-styling.css --lua-filter=pagebreak.lua 
-pandoc -s EBH.md -o out-EBH.pdf --toc 
+pandoc -s EBH.md -o out-EBH.pdf --toc -H PDF-latex-options.sty
