e6c18003210f2a2b15957cc6c84ff40dfbd93b70
Reinaldo Arrosi <rarrosi@gmail.com>
2020-01-21 11:18:43 -0300

    HTML version of book - part 3

--- styling/html-nav.js
+++ styling/html-nav.js
@@ -1,5 +1,5 @@
 (function() {
-    var toc, copyright, chapters, footnotes, currentPage;
+    var toc, copyright, chapters, sections, footnotes, currentPage, prevButton, nextButton;
 
     function supportsHistoryApi() {
         return !!(window.history && history.pushState);
@@ -23,33 +23,49 @@
         if (currentPage == null) {
             show(toc);
             show(copyright);
-            
+
             window.location.hash = '';
         } else {
             hide(toc);
             hide(copyright);
             show(currentPage);
 
-            if (currentPage.hash !== hash) {
-                window.location.hash = '';
-                window.location.hash = hash;
-            }
+            window.location.hash = hash;
+        }
+
+        if (currentPage == chapters[chapters.length - 1]) {
+            show(footnotes);
+        } else {
+            hide(footnotes);
+        }
+
+        prevButton.disabled = currentPage == null;
+        nextButton.disabled = currentPage == chapters[chapters.length - 1];
+    }
+
+    function goToPrevious() {
+        if(!currentPage) {
+            return;
+        } else {
+            var index = chapters.findIndex(function(x) { return x.hash == currentPage.hash });
+            index--;
+
+            window.location.hash = chapters[index] ? chapters[index].hash : '';
         }
     }
 
-    function onTocLinkClick(e) {
-        e.preventDefault();
-        goTo(e.target.hash);
+    function goToNext() {
+        var index = currentPage ? chapters.findIndex(function(x) { return x.hash == currentPage.hash }) : -1;
+        index++;
+
+        window.location.hash = chapters[index] ? chapters[index].hash : '';
     }
 
     function getRootChapter(hash) {
-        var chapter = chapters[hash];
+        var section = sections.find(function (x) { return x.hash === hash; });
+        while (section && section.parentChapter) { section = section.parentChapter;}
 
-        while (chapter && chapter.parentChapter) {
-            chapter = chapter.parentChapter;
-        }
-
-        return chapter;
+        return section;
     }
 
     function buildSubChapters(chapter, level) {
@@ -61,14 +77,30 @@
             
             buildSubChapters(x, level + 1);
 
-            chapters[x.hash] = x;
+            sections.push(x);
         });
     }
 
+    function setupShareButtons() {
+        var shareButtonsContainer = document.getElementById("share-buttons");
+        show(shareButtonsContainer);
+
+        var shareButtons = shareButtonsContainer.getElementsByTagName("a");
+        for(var i = 0; i < shareButtons.length; i++) {
+            var button = shareButtons[i];
+
+            button.href = button.href
+                .replace("{url}", encodeURI(window.location.protocol + "//" + window.location.host + window.location.pathname))
+                .replace("{title}", "Evidence-Based Hiring")
+        }
+    }
+
     function init() {
-        if (!supportsHistoryApi()) {
-            // if the browser doesn't support history api
-            // leave the book as it is, a single HTML file
+        setupShareButtons();
+
+        if (typeof (document.querySelector) !== "function") {
+            // if the browser doesn't the apis we need
+            // keep the book in a single HTML page
             return;
         }
 
@@ -79,14 +111,11 @@
         tocButton.addEventListener('click', function() { goTo(null); });
 
         toc = document.querySelector("#TOC");
-        toc.querySelectorAll('a').forEach(function(x) { x.addEventListener('click', onTocLinkClick); });
 
         copyright = document.querySelector("section#copyright"); 
 
-        footnotes = document.querySelector("section.footnotes"); 
-        footnotes.style.display = "none";
-
-        chapters = {};
+        sections = [];
+        chapters = [];
         document.querySelectorAll("section.level2")
             .forEach(function(x) { 
                 x.hash = '#' + x.id;
@@ -94,10 +123,21 @@
 
                 buildSubChapters(x, 3);
 
-                chapters[x.hash] = x;
+                sections.push(x);
+                chapters.push(x);
             });
 
+        footnotes = document.querySelector("section.footnotes"); 
+        footnotes.style.display = "none";
+
+        prevButton = document.getElementById("prev-button");
+        prevButton.addEventListener('click', goToPrevious);
+
+        nextButton = document.getElementById("next-button");
+        nextButton.addEventListener('click', goToNext);
+
         goTo(window.location.hash);
+        window.addEventListener('hashchange', function() { goTo(window.location.hash); });
     }
 
     init();
