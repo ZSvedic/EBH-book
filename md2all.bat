94d3fccabf7f165068516b58a32c871acbb1d200
Reinaldo Arrosi <rarrosi@gmail.com>
2020-01-21 11:43:31 -0300

    Undo temporary changes to md2all.bat

--- md2all.bat
+++ md2all.bat
@@ -2,7 +2,6 @@
 pushd source
 
 @REM To define only in one place, define some variables:
-set title=Evidence-Based Hiring
 set front=front-matter.md
 set content=chapter1.md chapter2.md chapter3.md chapter4.md chapter5.md chapter6.md chapter7.md chapter8.md bonus-chapter.md back-matter.md
 
@@ -10,27 +9,27 @@ set content=chapter1.md chapter2.md chapter3.md chapter4.md chapter5.md chapter6
 pandoc -s %front% %content% -o ../output/EBH.html --toc --section-divs -c ../styling/html-styling.css --lua-filter ../styling/pagebreak.lua --lua-filter ../styling/html-open-links-in-new-tab.lua --template ../styling/html-template.html
 
 @REM Build EPUB:
-@REM pandoc -s %front% %content% -o ../output/EBH.epub --toc -c ../styling/epub-styling.css --lua-filter ../styling/pagebreak.lua 
+pandoc -s %front% %content% -o ../output/EBH.epub --toc -c ../styling/epub-styling.css --lua-filter ../styling/pagebreak.lua 
 
 @REM Build print PDF:
-@REM pandoc -s %front% -o ../output/EBH-print-front.pdf --columns 55 -H ../styling/pdf-print-latex-options.sty --lua-filter ../styling/pdf-center-images.lua --template ../styling/pdf-print-front-template.tex
-@REM pandoc -s %content% -o ../output/EBH-print-content.pdf --columns 55 -H ../styling/pdf-print-latex-options.sty --lua-filter ../styling/pdf-center-images.lua --toc 
+pandoc -s %front% -o ../output/EBH-print-front.pdf --columns 55 -H ../styling/pdf-print-latex-options.sty --lua-filter ../styling/pdf-center-images.lua --template ../styling/pdf-print-front-template.tex
+pandoc -s %content% -o ../output/EBH-print-content.pdf --columns 55 -H ../styling/pdf-print-latex-options.sty --lua-filter ../styling/pdf-center-images.lua --toc 
 
-@REM pdflatex -job-name=EBH-print -output-directory=../output/ ../styling/pdf-print-merge.tex
-@REM del ..\output\EBH-print-front.pdf
-@REM del ..\output\EBH-print-content.pdf
-@REM del ..\output\EBH-print.aux
-@REM del ..\output\EBH-print.log
+pdflatex -job-name=EBH-print -output-directory=../output/ ../styling/pdf-print-merge.tex
+del ..\output\EBH-print-front.pdf
+del ..\output\EBH-print-content.pdf
+del ..\output\EBH-print.aux
+del ..\output\EBH-print.log
 
 @REM Build standalone PDF:
-@REM pandoc -s %front% -o ../output/EBH-standalone-front.pdf --columns 55 -H ../styling/pdf-standalone-latex-options.sty --lua-filter ../styling/pdf-center-images.lua --template ../styling/pdf-standalone-front-template.tex
-@REM pandoc -s %content% -o ../output/EBH-standalone-content.pdf --columns 55 -H ../styling/pdf-standalone-latex-options.sty --lua-filter ../styling/pdf-center-images.lua --toc
+pandoc -s %front% -o ../output/EBH-standalone-front.pdf --columns 55 -H ../styling/pdf-standalone-latex-options.sty --lua-filter ../styling/pdf-center-images.lua --template ../styling/pdf-standalone-front-template.tex
+pandoc -s %content% -o ../output/EBH-standalone-content.pdf --columns 55 -H ../styling/pdf-standalone-latex-options.sty --lua-filter ../styling/pdf-center-images.lua --toc
 
-@REM pdflatex -job-name=EBH-standalone -output-directory=../output/ ../styling/pdf-standalone-merge.tex
-@REM del ..\output\EBH-standalone-front.pdf
-@REM del ..\output\EBH-standalone-content.pdf
-@REM del ..\output\EBH-standalone.aux
-@REM del ..\output\EBH-standalone.log
+pdflatex -job-name=EBH-standalone -output-directory=../output/ ../styling/pdf-standalone-merge.tex
+del ..\output\EBH-standalone-front.pdf
+del ..\output\EBH-standalone-content.pdf
+del ..\output\EBH-standalone.aux
+del ..\output\EBH-standalone.log
 
 @REM Go back to the original folder:
-popd 
+popd
