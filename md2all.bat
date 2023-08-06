f49753e3561039f402206eacde81c8dfb7922cfa
Rander Oliveira <randergabriel98@gmail.com>
2023-08-05 21:13:36 -0300

    spliting the book into multiple HTML files

--- md2all.bat
+++ md2all.bat
@@ -5,8 +5,22 @@ pushd source
 set front=front-matter.md
 set content=chapter1.md chapter2.md chapter3.md chapter4.md chapter5.md chapter6.md chapter7.md chapter8.md bonus-chapter.md back-matter.md
 
+@REM Create folders:
+if not exist ../output mkdir ../output
+if not exist ../output/web mkdir ../output/web
+
 @REM Build HTML:
-pandoc -s %front% %content% -o ../output/ebh.html --toc --section-divs --lua-filter ../styling/pagebreak.lua --lua-filter ../styling/html-open-links-in-new-tab.lua --lua-filter ../styling/html-lazy-images.lua --template ../styling/html-template.html
+pandoc --shift-heading-level-by -1 --split-level 1 -t chunkedhtml -s %front% %content% -o ../output/web/ebh --toc --section-divs --lua-filter ../styling/pagebreak.lua --lua-filter ../styling/html-open-links-in-new-tab.lua --lua-filter ../styling/html-lazy-images.lua --template ../styling/html-template.html
+
+@REM Replacing index.html with licence.html:
+del ..\output\web\ebh\index.html
+rename ..\output\web\ebh\1-license.html index.html
+sed -i 's/1-license.html/index.html/g' ../output/web/ebh/*.html
+
+@REM Include styling and images in the output folder:
+cp -r ../styling ../output/web
+cp -r ../Amazon ../output/web
+cp -r ../images ../output/web
 
 @REM Build EPUB:
 pandoc -s %front% %content% -o ../output/ebh.epub --toc -c ../styling/epub-styling.css --lua-filter ../styling/pagebreak.lua 
